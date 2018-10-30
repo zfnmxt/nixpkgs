73f83b9a6a338ee7f36ff35e722a7f4148af894e
Ryan Trinkle <ryan.trinkle@gmail.com>
2018-10-30 12:24:57 -0400

    Add dynamic hairpin routing

--- nixos/modules/services/networking/nat.nix
+++ nixos/modules/services/networking/nat.nix
@@ -14,6 +14,7 @@ let
 
   flushNat = ''
     iptables -w -t nat -D PREROUTING -j nixos-nat-pre 2>/dev/null|| true
+    iptables -w -t nat -D OUTPUT -j nixos-nat-pre 2>/dev/null|| true
     iptables -w -t nat -F nixos-nat-pre 2>/dev/null || true
     iptables -w -t nat -X nixos-nat-pre 2>/dev/null || true
     iptables -w -t nat -D POSTROUTING -j nixos-nat-post 2>/dev/null || true
@@ -40,6 +41,8 @@ let
       iptables -w -t nat -A nixos-nat-post -m mark --mark 1 \
         ${optionalString (cfg.externalInterface != null) "-o ${cfg.externalInterface}"} ${dest}
     ''}
+      iptables -w -t nat -A nixos-nat-post -m mark --mark 1 \
+        -d 192.168.2.0/24 -j SNAT --to-source 192.168.2.1
 
     # NAT packets coming from the internal IPs.
     ${concatMapStrings (range: ''
@@ -50,8 +53,14 @@ let
     # NAT from external ports to internal ports.
     ${concatMapStrings (fwd: ''
       iptables -w -t nat -A nixos-nat-pre \
-        -i ${toString cfg.externalInterface} -p ${fwd.proto} \
-        --dport ${builtins.toString fwd.sourcePort} \
+        ${concatMapStrings (ip: "\! -d ${ip} ") cfg.internalIPs} \
+        -m addrtype --dst-type LOCAL \
+        -p ${fwd.proto} --dport ${builtins.toString fwd.sourcePort} \
+        -j MARK --set-mark 1
+      iptables -w -t nat -A nixos-nat-pre \
+        ${concatMapStrings (ip: "\! -d ${ip} ") cfg.internalIPs} \
+        -m addrtype --dst-type LOCAL \
+        -p ${fwd.proto} --dport ${builtins.toString fwd.sourcePort} \
         -j DNAT --to-destination ${fwd.destination}
 
       ${concatMapStrings (loopbackip:
@@ -89,6 +98,7 @@ let
 
     # Append our chains to the nat tables
     iptables -w -t nat -A PREROUTING -j nixos-nat-pre
+    iptables -w -t nat -A OUTPUT -j nixos-nat-pre
     iptables -w -t nat -A POSTROUTING -j nixos-nat-post
   '';
 
